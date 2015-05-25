b118efdb0fca81c29135083841910d16312f9307
lian <imwhatiam123@gmail.com>
2015-05-25 22:17:27 +0800

    add logger and test ip

--- mysite/settings.py
+++ mysite/settings.py
@@ -70,6 +70,39 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/tmp/mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+#        'django': {
+#            'handlers':['file'],
+#            'propagate': True,
+#            'level':'DEBUG',
+#        },
+        'zufang': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
--- zufang/views.py
+++ zufang/views.py
@@ -1,8 +1,24 @@
+import logging
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
+logger = logging.getLogger(__name__)
 # Create your views here.
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
 def zufang(request):
+
+    ip = get_client_ip(request)
+
+    logger.info(ip)
+
     return render_to_response('zufang_base.html', {
         }, context_instance=RequestContext(request))
